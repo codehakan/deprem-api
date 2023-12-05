e8af34c5fd60e2c62a186baf11f69c747880ccb9
Hakan AKKAYA <code.hakan@gmail.com>
2023-12-05 15:05:14 +0300

    Initial commit

---
+++ src/main/kotlin/net/hakanakkaya/Application.kt
@@ -0,0 +1,19 @@
+package net.hakanakkaya
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import net.hakanakkaya.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureSockets()
+    configureRouting()
+}
---
+++ src/main/kotlin/net/hakanakkaya/plugins/Routing.kt
@@ -0,0 +1,17 @@
+package net.hakanakkaya.plugins
+
+import io.ktor.http.*
+import io.ktor.resources.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.resources.*
+import io.ktor.server.resources.Resources
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+fun Application.configureRouting() {
+    
+}
+
